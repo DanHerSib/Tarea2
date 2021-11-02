059910088718e743231e90921cfa3390166d5d39
DanHerSib <Daniel@LAPTOP-TK7R86K2>
2021-11-01 23:24:44 -0600

    DataBase added

---
+++ Database/Database/dbo/StoreProcedures/TipoIdentificacionObtener.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[TipoIdentificacionObtener]
+      @IdTipoIdentificacion int= NULL
+AS BEGIN
+  SET NOCOUNT ON
+  SELECT 
+     T.IdTipoIdentificacion, T.Descripcion       
+    FROM dbo.TipoIdentificacion T
+    WHERE
+    (@IdTipoIdentificacion IS NULL OR IdTipoIdentificacion = @IdTipoIdentificacion)
+END
---
+++ Database/InitialData/ScriptConfiguration/04-TipoIdentificacion.sql
@@ -0,0 +1,25 @@
+﻿DROP TABLE IF EXISTS #TipoIdentificacionTemp
+
+SELECT 
+    IdTipoIdentificacion, Descripcion INTO #TipoIdentificacionTemp FROM 
+    (VALUES (1, 'Cedula'), (2, 'Pasaporte'), (3, 'Cedula Juridica')) 
+    AS TEMP(IdTipoIdentificacion, Descripcion)
+
+----ACTUALIZAR DATOS---
+UPDATE TI SET TI.Descripcion = TM.Descripcion
+    FROM Dbo.TipoIdentificacion TI
+        INNER JOIN #TipoIdentificacionTemp TM
+        ON TI.IdTipoIdentificacion = TM.IdTipoIdentificacion
+
+----INSERTAR DATOS---
+SET IDENTITY_INSERT dbo.TipoIdentificacion ON
+INSERT INTO dbo.TipoIdentificacion(IdTipoIdentificacion, Descripcion)
+    SELECT
+        IdTipoIdentificacion, Descripcion
+    FROM #TipoIdentificacionTemp
+EXCEPT
+    SELECT
+        IdTipoIdentificacion, Descripcion
+        FROM dbo.TipoIdentificacion
+SET IDENTITY_INSERT dbo.TipoIdentificacion OFF
+Go
